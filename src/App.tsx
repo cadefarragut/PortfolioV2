62d811f8b13b9f399383db94ee01278112d5309d
cadefarragut <cade_farragut@yahoo.com>
2025-08-14 22:33:17 -0500

    traffic tracker

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import routes from "tempo-routes";
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
